d1c6e67c265a32eb688f83fb69db7766f7b6fe5c
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2012-11-08 09:27:04 +0000

    .

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 $wants = "html";       
 $views = array(
 	        array( 'ext' => 'html', 'mimetypes' => array( 'text/html', 'application/xhtml+xml')),
-        	array( 'ext' => 'ttl', 'mimetypes' => array( 'text/turtle'))
+        	array( 'ext' => 'ttl', 'mimetypes' => array( 'text/turtle', 'application/rdf+xml'))
 );
 
 if( isset( $_SERVER["HTTP_ACCEPT"] ) )
@@ -39,5 +39,174 @@ if( isset( $_SERVER["HTTP_ACCEPT"] ) )
 if( $wants == "ttl" )
 {
 	header( "Location: http://openorg.ecs.soton.ac.uk/linkingyou/linkingyou.ttl" );
+	exit;
 }
-print "Human description";
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+?>
+<!DOCTYPE html>
+<html lang="en-uk">
+    <head>
+        <meta charset="utf-8">
+        <title>Linking-You Vocabulary</title>
+        <style>
+body,div,dl,dt,dd,ul,ol,li,h1,h2,h3,h4,h5,h6,pre,form,fieldset,input,textarea,p,blockquote,th,td { margin:0; padding:0; } table { border-collapse:collapse; border-spacing:0; } fieldset,img { border:0; } address,caption,cite,code,dfn,em,strong,th,var { font-style:normal; font-weight:normal; } ol,ul { list-style:none; } caption,th { text-align:left; } h1,h2,h3,h4,h5,h6 { font-size:100%; font-weight:normal; } q:before,q:after { content:''; } abbr,acronym { border:0; }
+html {
+	background-color: #333;
+	padding: 40px;
+}
+body {
+	background-color: #999;
+	padding: 0 20px 20px 20px;
+	border: solid 4px #000;
+	font-family: sans-serif;
+}
+h1 { 
+	font-size: 300%;
+	position: relative;
+	border: solid 4px #000;
+	padding: 12px;
+	background-color: #666666;
+	color: #fff;
+	display: inline-block;
+	left: -40px;
+	top: -20px;
+}	
+h2 {
+	background-color: #ff9933;
+	margin-left: -10px;
+	margin-top: 1em;
+	margin-bottom: 0.5em;
+	padding: 5px 10px 5px 10px;
+	border-right: solid 2px #000;
+	border-bottom: solid 2px #000;
+	font-size: 150%;
+	display: inline-block;
+}
+dl, p {
+	margin-top: 0.5em;
+	margin-bottom: 0.5em
+}
+dt {
+	border-right: solid 2px #000;
+	border-bottom: solid 2px #000;
+	display: inline-block;
+	padding: 4px;
+	background-color: #666666;
+	color: #fff;
+}	
+dd {
+	background-color: #c9c9c9;
+	border-right: solid 2px #000;
+	padding: 22px 4px 4px 4px;
+	border-bottom: solid 2px #000;
+	margin-left: 20px;
+	margin-top: -16px;
+	margin-bottom: 10px;
+}
+.uri {
+	margin-top: 0.5em;
+	font-size: 80%;
+}
+
+em {
+	font-style: italic;
+}
+.code {
+	white-space: pre;
+	font-family: monospace;
+}
+strong {
+	font-weight: bold;
+}
+        </style>
+    </head>
+    <body>
+        <h1>Linking-You RDF Vocabulary</h1>
+
+<p>Terms which link an organisation to common webpages, such as a contact page or "about" page.</p>      
+
+<p>A few changes have been made to terms to make them useful outside academia. It is generally expected that the targets will be HTML documents, but in some cases it may be other formats, such as a PDF prospectus.</p>
+
+<p>Namespace: <strong>http://purl.org/linkingyou/</strong></p>
+
+<p>This vocublary is based on the 
+<a href="http://lncn.eu/toolkit">Linking-You project</a> performed by those clever chaps at 
+Lincoln University. It has not been endorsed by them. The RDF version of the linking-you toolkit was created by <a href="http://users.ecs.soton.ac.uk/cjg/">Christopher Gutteridge</a>.</p>
+
+<p>View <a href='http://openorg.ecs.soton.ac.uk/linkingyou/linkingyou.ttl'>Linking-You Vocabulary</a> (RDF Turtle).</p>
+<p>For ease of consumption, the terms are split into the following categories.</p>
+<ul>
+<li><a href="#core">Core page types</a></li>
+<li><a href="#extended">Additional page types</a></li>
+<li><a href="#academia">Academic page types (not related to education)</a></li>
+<li><a href="#education">Page types related to education</a></li>
+
+<h2>Example of Use</h2>
+<div class='code'>
+&lt;http://id.southampton.ac.uk/&gt; 
+   foaf:homepage &lt;http://www.soton.ac.uk/&gt; ;
+   lyou:events &lt;http://www.events.soton.ac.uk/&gt; ;
+   lyou:research &lt;https://www.soton.ac.uk/research/index.shtml&gt; .
+</div>
+
+<?php
+render_vocab();
+?>
+    </body>
+</html>
+<?php
+
+exit;
+
+function render_vocab()
+{
+	require_once( "arc2/ARC2.php" );
+	require_once( "Graphite/Graphite.php" );
+	$graph = new Graphite();
+	$graph->load( "http://purl.org/linkingyou/" );
+	$graph->ns( "lyou", "http://purl.org/linkingyou/" );
+	
+	$nonacademic = array();
+	foreach( $graph->allOfType( "owl:ObjectProperty" ) as $property )
+	{
+		$nonacademic[ $property->toString() ] = $property;
+	}
+	$academic = $graph->resource( "http://purl.org/linkingyou/academia" )->all( "-dcterms:subject" );
+
+	# strip the non academic items from the nonacademic list	
+	foreach( $academic as $property ) 
+	{
+		unset( $nonacademic[ $property->toString() ] );
+	}
+
+	print "<h2>General Page Types</h2>";
+	print "<dl>";
+	foreach( $nonacademic as $property )
+	{
+		render_property( $property );
+	}
+	print "</dl>";
+
+	print "<h2>Academic Page Types</h2>";
+	print "<dl>";
+	foreach( $academic as $property )
+	{
+		render_property( $property );
+	}
+	print "</dl>";
+}
+
+function render_property( $property )
+{
+	print "<dt>Property: ".$property->g->shrinkURI( $property )."</dt>";
+	print "<dd>";
+	print "<em>".$property->label()."</em>";
+	if( $property->has( "rdfs:comment" ) )
+	{
+		print " - ".$property->getString( "rdfs:comment" );
+	}
+	print "<div class='uri'>URI: ".$property->toString()."</div>";
+	print "</dd>";
+}
